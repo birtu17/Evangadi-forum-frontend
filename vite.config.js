be4adf31195139070c64df2be0c44c880aaab3ad
birtu17 <birtukan2017@gmail.com>
2025-04-09 18:11:51 +0300

    backend on render and frontend on netlify config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      
+      "/api": {
+        target: "https://evangadi-forum-backend-2-77uy.onrender.com", 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), 
+      },
+    },
+  },
+});
